f881720b7288cda48e6120484e55c1240589028b
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2022-09-12 10:02:53 -0500

    Updated to Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Login from './features/auth/Login/Login';
 import Layout from './components/Layout'
 import Register from './features/auth/Register/Register';
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='login' element={<Login />} />
         <Route path='register' element={<Register />} />
@@ -18,7 +18,7 @@ function App() {
             </Route>
           </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
